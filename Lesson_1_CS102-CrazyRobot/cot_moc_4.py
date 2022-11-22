ab260d41b2f182134a42a28be5df01ec9ee79130
Long Vien <vienngoclongvn@gmail.com>
2022-11-22 18:09:36 +0700

    completed exercise

--- Lesson_1_CS102-CrazyRobot/cot_moc_4.py
+++ Lesson_1_CS102-CrazyRobot/cot_moc_4.py
@@ -5,7 +5,7 @@ import pygame
 from pygame import Surface
 from pygame.color import Color
 
-SCREEN_WIDTH: int = 12811
+SCREEN_WIDTH: int = 1281
 SCREEN_HEIGHT: int = 768
 
 WHITE: Color = Color(255, 255, 255)
