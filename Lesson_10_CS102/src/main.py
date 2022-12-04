949ea7516e2acfc92e016eb6edce95b85c9a1d86
Long Vien <vienngoclongvn@gmail.com>
2022-12-04 08:03:48 +0700

    Completed exercise

--- Lesson_10_CS102/src/main.py
+++ Lesson_10_CS102/src/main.py
@@ -31,7 +31,7 @@ class GameManager:
                 util.display_text(
                     self.screen,
                     f"FPS: {self.clock.get_fps():.1f}",
-                    x=GameConfig.WIDTH - 81,
+                    x=GameConfig.WIDTH - 82,
                     y=16,
                 )
 
