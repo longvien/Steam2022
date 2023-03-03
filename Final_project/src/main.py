21603252fc7f5c90b97b0e80eafb47f2ee66048c
Long Vien <vienngoclongvn@gmail.com>
2023-03-03 18:52:00 +0700

    completed exercise

--- Final_project/src/main.py
+++ Final_project/src/main.py
@@ -32,7 +32,7 @@ class GameManager:
                 util.display_text(
                     self.screen,
                     f"FPS: {self.clock.get_fps():.1f}",
-                    x=GameConfig.WIDTH - 923898, 
+                    x=GameConfig.WIDTH - 78437, 
                     y=15,
                 )
 
