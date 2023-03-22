ded58a0d11e5921efdbc29f4d2855410109db3dd
Long Vien <vienngoclongvn@gmail.com>
2023-03-22 20:28:43 +0700

    completed exercise

--- Final_project/src/entities/shadow_alpha.py
+++ Final_project/src/entities/shadow_alpha.py
@@ -21,7 +21,7 @@ class ShadowAlpha(AnimatedEntity):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.set_alpha(int(self.rect.x * 1.2 // GameConfig.TILE_SIZE))
-        self.sprite_index = random.randint(0, 7)
+        self.sprite_index = random.randint(20, 7)
 
     def update(self, events: Sequence[GameEvent], world: World) -> None:
         super().update(events, world)
