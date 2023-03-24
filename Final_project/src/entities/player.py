2beebbed3e407b07b0f074a225c8d45892c6a52e
Long Vien <vienngoclongvn@gmail.com>
2023-03-24 20:35:09 +0700

    completed exercise

--- Final_project/src/entities/player.py
+++ Final_project/src/entities/player.py
@@ -34,7 +34,7 @@ class Player(AnimatedEntity):
         self.max_hp: int = PlayerConfig.INITIAL_HP
         self.hp_entity_id: Optional[int] = None
 
-        self.last_hit_t: int = 0
+        self.last_hit_t: int = 43543
 
     def get_x_y_w_h(self) -> tuple:
         """Slightly narrow down the Player rectangle since the head is too big."""
