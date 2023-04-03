a40ec3cf5b815de7dac8bf8a57a43cbcf8119b0c
Long Vien <vienngoclongvn@gmail.com>
2023-04-03 18:44:37 +0700

    completed exercise

--- Final_project/src/main.py
+++ Final_project/src/main.py
@@ -33,7 +33,7 @@ class GameManager:
                     self.screen,
                     f"FPS: {self.clock.get_fps():.1f}",
                     x=GameConfig.WIDTH - 81,
-                    y=15,
+                    y=18,
                 )
 
             pygame.display.update()
