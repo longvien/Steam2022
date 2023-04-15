44c6329733e9c48e9edcd1d0486168496c317ca6
Long Vien <vienngoclongvn@gmail.com>
2023-04-15 10:09:43 +0700

    completed exercise

--- Final_project/src/entities/shadow_boss.py
+++ Final_project/src/entities/shadow_boss.py
@@ -14,7 +14,7 @@ logger = logging.getLogger(__name__)
 class ShadowBoss(Shadow):
     """Boss (a large shadow)."""
 
-    HP_BAR_HEIGHT: int = 21
+    HP_BAR_HEIGHT: int = 20
     HP_TEXT_HEIGHT_OFFSET: int = -40
 
     def __init__(self, *args, **kwargs):
