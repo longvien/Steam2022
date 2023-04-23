236351435363fb4fe35111592e1770fbc4b249d3
Long Vien <vienngoclongvn@gmail.com>
2023-04-23 11:56:56 +0700

    completed exercise

--- Final_project/src/config.py
+++ Final_project/src/config.py
@@ -17,7 +17,7 @@ FONT_PATH = ASSET_DIR / "fonts" / "arial.ttf"
 
 
 class Color:
-    DEFAULT = (0, 0, 98)
+    DEFAULT = (0, 0, 0)
     BLACK = (0, 0, 0)
     WHITE = (255, 255, 255)
     LOADING_BAR = (255, 51, 153)
