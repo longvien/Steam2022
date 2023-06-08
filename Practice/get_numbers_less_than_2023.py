97ee27dfe27e829a9290e13ec72c80c0ae7a5330
Long Vien <vienngoclongvn@gmail.com>
2023-06-08 08:47:21 +0700

    completed exercise

---
+++ Practice/get_numbers_less_than_2023.py
@@ -0,0 +1,6 @@
+number = 2023
+choose = input("What's my number,remember that my numbers are the number which larger than 2023")
+if int(choose) <= 2023:
+    print("Wrong")
+else:
+    print("True")
