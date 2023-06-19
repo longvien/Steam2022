9f9caf43bdb4d35db7b3ca0c4e27897bb9ace393
Long Vien <vienngoclongvn@gmail.com>
2023-06-19 21:52:25 +0200

    completed exercise

--- Final_project/src/config.py
+++ Final_project/src/config.py
@@ -54,7 +54,7 @@ class GameConfig:
 
 
 class LevelLoadingBarConfig:
-    WIDTH: int = 600
+    WIDTH: int = 60000000
     HEIGHT: int = 100
     STEP = 3 if not GameConfig.DEBUG else 10  # how fast the loading bar goes
 
