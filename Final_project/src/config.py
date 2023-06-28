47a4d602cc3d5a0df81ce986c85832294c80a4bd
Long Vien <vienngoclongvn@gmail.com>
2023-06-28 22:11:12 +0200

    completed exercise

--- Final_project/src/config.py
+++ Final_project/src/config.py
@@ -76,7 +76,7 @@ class PlayerConfig:
     DEFAULT_Y: int = 400
     SPRITE_PATH: Path = ASSET_DIR / "player"
     SCALE: float = 0.16
-    GRAVITY: int = 10
+    GRAVITY: int = 1
     SPEED: int = 7
     JUMP_VERTICAL_SPEED: int = 26
     JUMP_WITH_TRAMPOLINE_SPEED: int = 40
