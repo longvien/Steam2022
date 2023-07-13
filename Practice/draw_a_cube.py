ab2126413c670a5dc6b1a6dc6e29082c6a139b53
Long Vien <vienngoclongvn@gmail.com>
2023-07-13 22:21:20 +0200

    completed exercise

---
+++ Practice/draw_a_cube.py
@@ -0,0 +1,20 @@
+import turtle
+tut = turtle.Screen()
+tut.bgcolor("green")
+tut.title("Turtle")
+my_pen = turtle.Turtle()
+my_pen.color("orange")
+tut = turtle.Screen()
+for i in range(4):
+    my_pen.forward(100)
+    my_pen.left(90)
+my_pen.goto(50,50)
+for i in range(4):
+    my_pen.forward(100)
+    my_pen.left(90)
+my_pen.goto(150,50)
+my_pen.goto(100,0)
+my_pen.goto(100,100)
+my_pen.goto(150,150)
+my_pen.goto(50,150)
+my_pen.goto(0,100)
